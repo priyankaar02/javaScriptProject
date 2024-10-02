1b95c4b118eb305908743f78007f8259b3de425b
priyankaar02 <priyankaar02@gmail.com>
2024-10-02 16:17:58 +0200

    Array

---
+++ Notes2.js
@@ -0,0 +1,7 @@
+/****************ARRAY ******************************/
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myCountries = ["Germany", "India", "Switzerland", "USA" ];
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr2[1]);
