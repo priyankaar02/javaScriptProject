427d6b9a31b2563e77ed8897e1c5a057a9e04306
priyankaar02 <priyankaar02@gmail.com>
2024-10-04 17:00:37 +0200

    Functions

---
+++ Funtions.js
@@ -0,0 +1,67 @@
+
+function sayMyName() {
+    // console.log("p");
+    // console.log("r");
+    // console.log("i");
+    // console.log("y");
+    // console.log("a");
+}
+
+sayMyName()
+
+function addTwoNumbers(number1,number2){
+    return number1+number2;
+}
+
+const result = addTwoNumbers(3,4)
+//console.log("Result:", result);
+
+function loginUserMessage(username){
+    if(!username) {
+        console.log("please enter a username");
+        return
+    }
+    return `User ${username} just logged in`
+}
+//console.log(loginUserMessage("priya"))
+//console.log(loginUserMessage())
+
+/* ---------------REST Operator(...) ----------------*/
+ function calculateCartPrice(...num1) {
+    return num1;
+ }
+ //console.log(calculateCartPrice(100,200,300));
+
+ function calculateCartPrice2(val1, val2,...num2){
+    return num2;
+ }
+ //console.log(calculateCartPrice2(100,200,300,1000));
+
+ /*-------Passing Objects to functions as parameter --------------*/
+
+const userObj = {
+    username: "Sam",
+    price: 2000
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and the price is ${anyObject.price}`); 
+}
+//handleObject(userObj) /* Method 1 */
+handleObject({          /* Method 2 */
+    username: "Sam",
+    price: 2000
+})
+
+/*----------------- Passing Array to a function ----------------*/
+
+const myArr = [100, 200, 300, 400]
+function returnArrayValue(getArray) {
+    return getArray[2]
+}
+//console.log(returnArrayValue(myArr));                /* Method 1 */
+console.log(returnArrayValue([100, 200, 300, 400]))    /* Method 2 */
+
+
+
+
