1fa8ea1b8e0f06711b41c8cffb368436a44740ec
priyankaar02 <priyankaar02@gmail.com>
2024-10-04 15:32:10 +0200

    Removed index and notes2 file

--- Notes2.js
+++
@@ -1,7 +0,0 @@
-/****************ARRAY ******************************/
-
-const myArr = [0, 1, 2, 3, 4, 5]
-const myCountries = ["Germany", "India", "Switzerland", "USA" ];
-
-const myArr2 = new Array(1,2,3,4)
-console.log(myArr2[1]);
