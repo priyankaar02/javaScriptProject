3bbc03235593f1e2f06a65d445a5c8fdefa83ff7
priyankaar02 <55306664+priyankaar02@users.noreply.github.com>
2025-01-14 08:37:12 +0100

    Create variables file

---
+++ variables.js
@@ -0,0 +1,14 @@
+/* In javascript, variables can be declared using const, let, var */
+
+const accountId = 144553;
+let accountEmail = "sam123@gmail.com";
+var accountPassword = "12345";
+accountCity = "Bangalore";
+
+// accountId = 2;  Assignment to constant variable is not allowed
+console.log(accountId);
+
+accountEmail = "john123@gmail.com";
+accountPassword = "212121";
+accountCity = "Pune";
+console.table([accountId, accountEmail, accountPassword, accountCity]);
