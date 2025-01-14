23d5bf1ebc4b701361d2dbed1fa419542bb8ef00
priyankaar02 <55306664+priyankaar02@users.noreply.github.com>
2025-01-14 12:07:22 +0100

    Create Operations.js

---
+++ Operations.js
@@ -0,0 +1,22 @@
+//---------------------OPERATIONS-----------------------------------------
+
+let value = 3;
+let negValue = -value;
+console.log(negValue); //-3
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+let strg = "hello";
+let strg2 = "John";
+let strg3 = strg + strg2;
+console.log(strg3); //hello John
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + "2"); //
+console.log("1" + 2  + 2); //122 
+console.log(1 + 2  + "2"); //32
