6b0b19ae08c7e3ea8338346297c55e9fb939c638
priyankaar02 <55306664+priyankaar02@users.noreply.github.com>
2025-01-14 12:50:53 +0100

    Update Operations.js

--- Operations.js
+++ Operations.js
@@ -20,3 +20,21 @@ console.log(1 + "2"); //12
 console.log("1" + "2"); //
 console.log("1" + 2  + 2); //122 
 console.log(1 + 2  + "2"); //32
+
+
+console.log(true); //true
+console.log(+true); //1
+//console.log(true+); error
+console.log(+""); //0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); //101
+
+let gameCounter1 = 100;
+++gameCounter1++;
+console.log(gameCounter); //100
+
