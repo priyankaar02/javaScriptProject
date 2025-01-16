8196b547042e6b1a1f58e11e8fd701d50904a03f
priyankaar02 <55306664+priyankaar02@users.noreply.github.com>
2025-01-16 10:11:27 +0100

    Update FunctionExercises.js

--- FunctionExercises.js
+++ FunctionExercises.js
@@ -22,6 +22,44 @@ let orderInformation = orderTea("chai");
 console.log(orderInformation);
 
 /* Question 3
-Write an arrow  function name `orderTea` that takes one parameter, `teaType`, and inside this function create another function named `confirmOrder` that returns a message like `"Order confirmed for chai"` Call `confirmOrder` 
-from within `orderTea` and return the result.
+Write an arrow  function named `calculateTotal` that takes two parameters, `price` and `quantity`. The function should return a total cost by multiplying the `price` and `quantity`. Store the result in a variable named
+`totalCost`
 */
+/*-----METHOD 1-----*/
+const calculateTotal = (price, quantity) => {
+  return price * quantity;
+}
+let totalCost = calculateTotal(500, 5);
+
+/*-----METHOD 2-----*/
+const calculateTotal = (price, quantity) => price * quantity;
+let totalCost1 = calculateTotal(500, 5);
+
+/* NOTE: THIS => is a context. In the browser, window object is a context.
+Arrow function has `this` but it does not have enough ability in the arrow function ie cannot pass on this context. */
+ 
+/* Question 4: HIGHER ORDER FUNCTION (First-Class Function) [Passing a function as A parameter inside another function is higher order function. Example: forEach loop]
+Write a function named `processTeaOrder` that takes another function, `makeTea` as the parameter and calls it with the argumnent `"earl grey"`. Return the result of calling `makeTea`
+*/
+function makeTea(typeOfTea) {
+  return `maketea: ${typeOfTea}`
+}
+function processTeaOrder(teaFunction) {
+  return teaFunction('earl grey');
+}
+let order = processTeaOrder(makeTea);
+console.log(order);
+
+/* Question 5: NESTED FUNCTION [link: https://www.geeksforgeeks.org/javascript-nested-functions/]
+Write a function named `createTeaMaker` that returns another function. The returned function should take one parameter, `teaType` and return a message like `Making green tea`.
+Store the returned function in a variable name `teaMaker` and call it with `"green tea"` */
+
+function createTeaMaker(name) {
+  let score = 100;
+  return anotherFunction(teaType) {
+    return `Making ${teaType}, ${name} with ${score}`
+  };
+}
+let teaMaker = createTeaMaker('priyanka');
+let result = teaMaker('green tea')
+console.log(result);
