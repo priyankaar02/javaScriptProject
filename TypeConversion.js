0eb3edb5923e336b743d050ad1609e95dd3c248a
priyankaar02 <55306664+priyankaar02@users.noreply.github.com>
2025-01-16 15:13:20 +0100

    Update TypeConversion.js

--- TypeConversion.js
+++ TypeConversion.js
@@ -1,5 +1,12 @@
 //--------------------------------------CONVERSION OF DATATYPES-------------------------------------------------
-
+/*
+Converting Strings to Numbers
+Converting Numbers to Strings
+Converting Dates to Numbers
+Converting Numbers to Dates
+Converting Booleans to Numbers
+Converting Numbers to Booleans
+*/
 
 //--------------- Number Conversion----------------
 let score = 33;
